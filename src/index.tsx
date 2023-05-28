12636b0556cd335e6a10496efd988f0e1714f48b
SlavaOST <slavaost-it@mail.ru>
2023-05-28 21:49:56 +0300

    fix: change HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 import {App} from "./app/App";
@@ -12,10 +12,10 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App/>
         <GlobalStyled theme={theme}/>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 reportWebVitals();
