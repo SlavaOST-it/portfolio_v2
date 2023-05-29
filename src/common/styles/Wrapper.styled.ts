a9e5cb1f4ca89964fc6099bba590818903740d82
SlavaOST <slavaost-it@mail.ru>
2023-05-29 23:04:03 +0300

    fix: rename tags

--- src/common/styles/Wrapper.styled.ts
+++ src/common/styles/Wrapper.styled.ts
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 import {theme} from "./Theme.styled";
 
+
+export const TitlePage = styled.h2`
+  font-size: 48px;
+  line-height: 60px;
+  font-weight: 300;
+  margin-bottom: 33px;
+`
+
 export const Wrapper = styled.section`
   min-height: 100%;
   margin-bottom: 150px;
 
-  h1{      
+  h1 {
     font-size: 78px;
     letter-spacing: -0.2px;
     line-height: 90px;
     margin-bottom: 43px;
     font-weight: 300;
-  }
-
-  h2{
-    font-size: 48px;
-    line-height: 60px;
-    font-weight: 300;
-    margin-bottom: 33px;
   }
 
   span {
