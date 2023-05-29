5abd2c49bb6c199798eb42bd80765fa752b505af
SlavaOST <slavaost-it@mail.ru>
2023-05-29 23:09:59 +0300

    fix: delete HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 import {App} from "./app/App";
@@ -12,10 +11,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <HashRouter>
+    <>
         <App/>
         <GlobalStyled theme={theme}/>
-    </HashRouter>
+    </>
+
 );
 
 reportWebVitals();
