ecbba676679f661c348ca8f9a46e270d1ca50dfe
SlavaOST <slavaost-it@mail.ru>
2023-06-07 18:27:47 +0300

    feat: container

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -21,8 +21,11 @@ export const App = () => {
 }
 
 const AppWrapper = styled.div`
-
+  max-width: 1700px;
+  padding: 0 10px;
+  margin: 0 auto;
   
+
   @media screen and ${theme.media.mobile} {
     padding: 0 5px;
   }
