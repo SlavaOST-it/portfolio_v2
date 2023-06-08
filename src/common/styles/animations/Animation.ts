dbc02bdd1d4cb3c3831b5b32db920725d400fcae
SlavaOST <slavaost-it@mail.ru>
2023-06-08 20:15:02 +0300

    feat: add animation

--- src/common/styles/animations/Animation.ts
+++ src/common/styles/animations/Animation.ts
@@ -16,5 +16,19 @@ export const hiddenDescription = keyframes`
   }
   to {
     opacity: 0;
-  } 
-    `
+  }
+`
+
+
+//============ For Loader ================//
+export const dotLoader = keyframes`
+  50% {
+    transform: translateX(96px);
+  }
+`
+
+export const dotsLoader = keyframes`
+  50% {
+    transform: translateX(-31px)
+  }
+`
